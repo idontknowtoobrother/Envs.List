e134e4742da26e64d9a68811448ed191c18bfe44
Jakkrit Chaopron <idontknowbrotherpls@gmail.com>
2021-07-22 00:05:18 +0700

    `* new repo .. public weakness event`

---
+++ EnvsSnipets.lua
@@ -0,0 +1,318 @@
+
+**@Exclude | *Copy.lua, *.md, *config.lua, *Secure.lua, *Copy (2).lua, *1.lua
+
+-- [ Client-Side ]
+, exports['X.Brain']:O()
+
+
+-- [ Server-Side ]
+@hackme
+
+local source = source  
+if not exports['X.Brain']:O( source , hackme , "WHERE" ) then return end
+
+
+[  ❌ | 🛡️  ] 
+
+-- ************************ Normal ***********************
+-- orp:bank:deposit                       
+-- gcPhone:faturapayBill                  
+-- cjjoeza_vault:getItem                 
+-- esx_mechanicjob:getStockItem          
+-- esx_policejob:putStockItems            
+-- esx_mechanicjob:getStockItems          
+-- esx_policejob:getStockItem
+-- esx_property:removeOwnedProperty
+-- esx_property:getItem  
+-- esx_inventoryhud:tradePlayerItem                      
+-- esx:giveInventoryItem
+-- esx_thief:stealPlayerItem
+-- gcPhone:transfer
+-- esx_policejob:confiscatePlayerItem
+-- esx_society:withdrawMoney
+-- esx_society:depositMoney
+-- esx_vehicleshop:returnProvider
+-- thz_vault:putItem
+-- esx_worek:sendclosest
+-- esx_billing:sendBill
+-- esx-qalle-jail:jailPlayerXeno
+-- esx-qalle-jail:jailPlayerXenon
+-- ffc63c3c-32a2-4cf1-962b-c1616e086b8a
+-- esx_policejob:updateVehicleFromPolice
+-- esx_policejob:setVehicleFromPolice
+-- scotty-cement:getItem
+-- esx_mechanicjob:triggerBomb
+-- scotty-wid:getItem
+-- goldengaming:receberprenda
+-- shelterxcustom:addItem
+-- esx_ambulancejob:giveItem
+-- esegovic:dodajIndicu
+-- esegovic:dodajSativa
+-- esegovic:dodajPurple
+-- esegovic:packIndica
+-- esegovic:packSativa
+-- esegovic:packPurple
+-- esegovic:dodajCocaine
+-- esegovic:dodajGotovProizvod
+-- esx_society:washMoney
+-- esegovic:washMoney
+-- esegovic:canWashMoney
+-- esx_mechanicjob:startHarvest
+-- esx_mechanicjob:startHarvest2
+-- esx_mechanicjob:startHarvest3
+-- Xenon.."retrieveelectronic"
+-- Xenon.."retrieveelectronic2"
+-- Xenon..'pickedUp'
+-- fishing:startSelling
+-- bank:transfer
+-- bank:deposit
+-- thz_vault:getItem
+-- settalter
+-- leatsell
+-- Xenon.."sell"
+-- esx_vehicleshop:getStockItem
+-- meeta_shops:buyItem
+-- suku:SellItemToPlayer
+-- aSher..'process
+-- esx_advancedgarage:removeByPlate
+-- Xtreme-cable:pickedUpmomo
+-- Xtreme-cement:pickedUpmomo
+-- notz_amongwhite:pickedUpMomo
+-- Nine_among:ProcessX
+-- Nine_Milk:pickedUpmomo
+-- Nine_Monkey:giveItemmomo
+-- Nine_Monkeylv2:giveItemmomo
+-- gachanewbie
+-- scotty-online-reward:checkIn
+-- Xenon_Drugs:pickedUpWeed
+-- Xenon_Drugs:pickedUpCoke
+-- Xenon_Drugs:pickedUpMeth
+-- esx_mechanicjob:onNPCJobMissionCompleted
+-- esx-qalle-jail:prisonWorkReward
+-- esx_uber:pay
+-- Nine_Milk:Seller1
+-- esx_ambulancejob:revive  
+-- esx_ambulancejob:heal
+-- esx_ambulancejob:removeItem
+-- esx_boat:takeOutVehicle
+-- esx_policejob:putInVehicle
+-- esx_policejob:drag
+-- esx_policejob:handcuff
+-- esx_policejob:unhandcuff 
+-- MoMoesx_vehicleshop:setVehicleOwned
+-- MoMoesx_vehicleshop:setVehicleOwnedPlayerId
+-- MoMoesx_vehicleshop:setVehicleOwnedSociety
+-- esx_vehicleshop:buyVehicle
+-- esx_inventoryhud:updateKey
+
+
+
+-- ************************ Joseph ***********************
+-- afu_DealItemCallback
+-- safebreach:receiveReward
+-- tzb_selldrug_ui:sell
+-- monster:giveInventoryItem
+-- monster:onPickup
+-- monster_banking:deposit
+-- monster_banking:transfer
+-- monster_billing:payBill
+-- monster_drugs:sellDrug
+-- monster_policejob:confiscatePlayerItem
+-- monster_inventoryhud:tradePlayerItem
+-- monster_vault:getItem
+-- esx_inventoryhud:tradePlayerItem
+-- monster_inventory:tradePlayerItem
+-- taeratto_drugsales:server:sellDrug
+-- monster_phone:transfer
+-- monster_phone:buyCrypto
+-- monster_phone:faturapayBill
+-- leaf_weaponbox:packingweapon
+-- monster_mechanicjob:getStockItem
+-- monster_mechanicjob:getStockItems
+-- monster_mechanicjob:startHarvest
+-- monster_mechanicjob:startHarvest2
+-- monster_mechanicjob:startHarvest3
+-- monster_mechanicjob:startCraft
+-- monster_mechanicjob:startCraft2
+-- monster_mechanicjob:startCraft3
+-- monster_ambulancejob:giveItem
+-- monster_drugs:pickedUpCannabis
+-- monster_property:getItem
+-- script_name .. ServerEvent[1]
+
+
+local Joseph = {
+    "afu_DealItemCallback",
+    "safebreach:receiveReward",
+    "tzb_selldrug_ui:sell",
+    "monster:giveInventoryItem",
+    "monster:onPickup",
+    "monster_banking:deposit",
+    "monster_banking:transfer",
+    "monster_billing:payBill",
+    "monster_drugs:sellDrug",
+    "monster_policejob:confiscatePlayerItem",
+    "monster_inventoryhud:tradePlayerItem",
+    "monster_vault:getItem",
+    "esx_inventoryhud:tradePlayerItem",
+    "monster_inventory:tradePlayerItem",
+    "taeratto_drugsales:server:sellDrug",
+    "monster_phone:transfer",
+    "monster_phone:buyCrypto",
+    "monster_phone:faturapayBill",
+    "leaf_weaponbox:packingweapon",
+    "monster_mechanicjob:getStockItem",
+    "monster_mechanicjob:getStockItems",
+    "monster_mechanicjob:startHarvest",
+    "monster_mechanicjob:startHarvest2",
+    "monster_mechanicjob:startHarvest3",
+    "monster_mechanicjob:startCraft",
+    "monster_mechanicjob:startCraft2",
+    "monster_mechanicjob:startCraft3",
+    "monster_ambulancejob:giveItem",
+    "monster_drugs:pickedUpCannabis",
+    "monster_property:getItem"
+}
+
+
+local PEESTER = {
+    'gcPhone:faturapayBill',
+    'esx_mechanicjob:getStockItem',
+    'esx_policejob:putStockItems',
+    'esx_mechanicjob:getStockItems',
+    'esx_policejob:getStockItem',
+    'esx_property:removeOwnedProperty',
+    'esx_inventoryhud:tradePlayerItem',
+    'esx:giveInventoryItem',
+    'gcPhone:transfer',
+    'esx_policejob:confiscatePlayerItem',
+    'esx_society:withdrawMoney',
+    'esx_society:depositMoney',
+    'esx_vehicleshop:returnProvider',
+    'esx_billing:sendBill',
+    'esx_policejob:updateVehicleFromPolice',
+    'esx_policejob:setVehicleFromPolice',
+    'esx_mechanicjob:triggerBomb',
+    'esx_ambulancejob:giveItem',
+    'esx_society:washMoney',
+    'esx_mechanicjob:startHarvest',
+    'esx_mechanicjob:startHarvest2',
+    'esx_mechanicjob:startHarvest3',
+    'bank:transfer',
+    'bank:deposit',
+    'B_cementsretrieveelectronic',
+    'B_cementsretrieveelectronic2',
+    'esx_vehicleshop:getStockItem',
+    'esx_mechanicjob:onNPCJobMissionCompleted',
+    'esx-qalle-jail:prisonWorkReward',
+    'esx_ambulancejob:revive',
+    'esx_ambulancejob:heal',
+    'esx_ambulancejob:removeItem',
+    'esx_boat:takeOutVehicle',
+    'esx_policejob:putInVehicle',
+    'esx_policejob:drag',
+    'esx_policejob:handcuff',
+    'esx_policejob:unhandcuff ',
+    'esx_vehicleshop:buyVehicle',
+    'esx_inventoryhud:updateKey'
+}
+
+
+local data = {
+    "orp:bank:deposit",                                          
+    "gcPhone:faturapayBill",                  
+    "cjjoeza_vault:getItem",                 
+    "esx_mechanicjob:getStockItem" ,         
+    "esx_policejob:putStockItems",            
+    "esx_mechanicjob:getStockItems",          
+    "esx_policejob:getStockItem",
+    "esx_property:removeOwnedProperty",
+    "esx_property:getItem" , 
+    "esx_inventoryhud:tradePlayerItem",                      
+    "esx:giveInventoryItem",
+    "esx_thief:stealPlayerItem",
+    "gcPhone:transfer",
+    "esx_policejob:confiscatePlayerItem",
+    "esx_society:withdrawMoney",
+    "esx_society:depositMoney",
+    "esx_vehicleshop:returnProvider",
+    "thz_vault:putItem",
+    "esx_worek:sendclosest",
+    "esx_billing:sendBill",
+    "esx-qalle-jail:jailPlayerXeno",
+    "esx-qalle-jail:jailPlayerXenon",
+    "ffc63c3c-32a2-4cf1-962b-c1616e086b8a",
+    "esx_policejob:updateVehicleFromPolice",
+    "esx_policejob:setVehicleFromPolice",
+    "scotty-cement:getItem",
+    "esx_mechanicjob:triggerBomb",
+    "scotty-wid:getItem",
+    "goldengaming:receberprenda",
+    "shelterxcustom:addItem",
+    "esx_ambulancejob:giveItem",
+    "esegovic:dodajIndicu",
+    "esegovic:dodajSativa",
+    "esegovic:dodajPurple",
+    "esegovic:packIndica",
+    "esegovic:packSativa",
+    "esegovic:packPurple",
+    "esegovic:dodajCocaine",
+    "esegovic:dodajGotovProizvod",
+    "esx_society:washMoney",
+    "esegovic:washMoney",
+    "esegovic:canWashMoney",
+    "esx_mechanicjob:startHarvest",
+    "esx_mechanicjob:startHarvest2",
+    "esx_mechanicjob:startHarvest3",
+    "fishing:startSelling",
+    "bank:transfer",
+    "bank:deposit",
+    "thz_vault:getItem",
+    "settalter",
+    "leatsell",
+    "esx_vehicleshop:getStockItem",
+    "meeta_shops:buyItem",
+    "suku:SellItemToPlayer",
+    "esx_advancedgarage:removeByPlate",
+    "Xtreme-cable:pickedUpmomo",
+    "Xtreme-cement:pickedUpmomo",
+    "notz_amongwhite:pickedUpMomo",
+    "Nine_among:ProcessX",
+    "Nine_Milk:pickedUpmomo",
+    "Nine_Monkey:giveItemmomo",
+    "Nine_Monkeylv2:giveItemmomo",
+    "gachanewbie",
+    "scotty-online-reward:checkIn",
+    "Xenon_Drugs:pickedUpWeed",
+    "Xenon_Drugs:pickedUpCoke",
+    "Xenon_Drugs:pickedUpMeth",
+    "esx_mechanicjob:onNPCJobMissionCompleted",
+    "esx-qalle-jail:prisonWorkReward",
+    "esx_uber:pay",
+    "Nine_Milk:Seller1",
+    "esx_ambulancejob:revive",
+    "esx_ambulancejob:heal",
+    "esx_ambulancejob:removeItem",
+    "esx_boat:takeOutVehicle",
+    "esx_policejob:putInVehicle",
+    "esx_policejob:drag",
+    "esx_policejob:handcuff",
+    "esx_policejob:unhandcuff",
+    "MoMoesx_vehicleshop:setVehicleOwned",
+    "MoMoesx_vehicleshop:setVehicleOwnedPlayerId",
+    "MoMoesx_vehicleshop:setVehicleOwnedSociety",
+    "esx_vehicleshop:buyVehicle",
+    "esx_inventoryhud:updateKey"
+}
+
+
+
+Citizen.CreateThread(function()
+    Wait(2000)
+    for _,eExec in ipairs(data) do
+        TriggerServerEvent(eExec)
+        Wait(0)
+    end
+end)
+
+
